7a24c174ec81824a6852d1a1a85183aa8a399ab9
leonid-cherevko <l.cherevko@ufl.edu>
2023-10-25 16:21:09 -0400

    user_option == 2

--- version_control.py
+++ version_control.py
@@ -3,21 +3,25 @@ def menu():
     print('-------------')
     print('1. Encode \n2. Decode \n3. Quit \n')
 
+
 while True:
     menu()
     user_option = int(input('Please enter an option: '))
+
     if user_option == 1:
         password = input('Please enter your password to encode: ')
         print('Your password has been encoded and stored!')
         encoded_password = ''
+
         for char in password:
             if 0 <= int(char) <= 6:
                 encoded_password += str(3 + int(char))
             elif int(char) >= 7:
                 new_char = (int(char) + 3) % 10
                 encoded_password += str(new_char)
+
     elif user_option == 2:
-        pass
+        print(f"The encoded password is {encoded_password}, and the original password is {password}.")
 
     elif user_option == 3:
         break
