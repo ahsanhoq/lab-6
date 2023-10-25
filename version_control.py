e94105811179d84b61ba0af5fc1cf16bb2be4328
Ahsan Hoq <ahsan.hoq04@gmail.com>
2023-10-25 16:10:32 -0400

    hi

--- version_control.py
+++ version_control.py
@@ -1,1 +1,23 @@
-# hello00
+def menu():
+    print('Menu')
+    print('-------------')
+    print('1. Encode \n2. Decode \n3. Quit \n')
+
+while True:
+    menu()
+    user_option = int(input('Please enter an option: '))
+    if user_option == 1:
+        password = input('Please enter your password to encode: ')
+        print('Your password has been encoded and stored!')
+        encoded_password = ''
+        for char in password:
+            if 0 <= int(char) <= 6:
+                encoded_password += str(3 + int(char))
+            elif int(char) >= 7:
+                new_char = (int(char) + 3) % 10
+                encoded_password += str(new_char)
+    elif user_option == 2:
+        pass
+
+    elif user_option == 3:
+        break
